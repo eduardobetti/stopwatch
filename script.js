016ba31c0033675742fad7a17d92b026265782fa
eduardobetti <esbetti92@gmail.com>
2024-04-12 00:55:13 -0300

    alteração de nomenclaturas para inglês

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-const parar = document.getElementById('stop')
-const iniciar = document.getElementById('start')
-const resetar = document.getElementById('reset')
+const varStop = document.getElementById('stop')
+const varStart = document.getElementById('start')
+const varReset = document.getElementById('reset')
 let seconds = 0
 let minutes = 0
 let hours = 0
 let cron
-let segundos = document.getElementById('segundos')
-let minutos = document.getElementById('minutos')
-let horas = document.getElementById('horas')
+let pSeconds = document.getElementById('pSeconds')
+let pMinutes = document.getElementById('pMinutes')
+let pHours = document.getElementById('pHours')
 
 function start() {
-    iniciar.disabled = true;
-    parar.disabled = false;
-    resetar.disabled = true;
+    varStart.disabled = true;
+    varStop.disabled = false;
+    varReset.disabled = true;
     cron = setInterval(function () {
         seconds++
-        segundos.innerHTML = seconds.toString().padStart(2, '0');
+        pSeconds.innerHTML = seconds.toString().padStart(2, '0');
         if (seconds === 60) {
             seconds = 0
-            segundos.innerHTML = `00`
+            pSeconds.innerHTML = `00`
             minutes++
-            minutos.innerHTML = minutes.toString().padStart(2, '0');
+            pMinutes.innerHTML = minutes.toString().padStart(2, '0');
         } else if (minutes === 60) {
             minutes = 0
-            minutos.innerHTML = `00`
+            pMinutes.innerHTML = `00`
             hours++
-            horas.innerHTML = hours.toString().padStart(2, '0');
+            pHours.innerHTML = hours.toString().padStart(2, '0');
         }
     }, 1000);
 }
 
 function stop() {
-    iniciar.disabled = false;
-    parar.disabled = true;
-    resetar.disabled = false;
+    varStart.disabled = false;
+    varStop.disabled = true;
+    varReset.disabled = false;
     clearInterval(cron)
 }
 
 function reset() {
-    iniciar.disabled = false;
-    parar.disabled = true;
-    resetar.disabled = true;
-    seconds = 0; // prática (o número que o sistema enxerga)
-    segundos.innerHTML = `00` // estética (o número que aparece)
+    varStart.disabled = false
+    varStop.disabled = true
+    varReset.disabled = true
+    seconds = 0
+    pSeconds.innerHTML = `00`
     minutes = 0
-    minutos.innerHTML = `00`
+    pMinutes.innerHTML = `00`
     hours = 0
-    horas.innerHTML = `00`
+    pHours.innerHTML = `00`
 }
